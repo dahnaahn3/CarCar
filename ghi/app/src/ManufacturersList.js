075ed5f8931af9e9ae59deb90a65451f4ee63601
Matthew Huff <mhuff9112@gmail.com>
2023-04-24 19:28:06 -0400

    completed new manufacturer form.

--- ghi/app/src/ManufacturersList.js
+++ ghi/app/src/ManufacturersList.js
@@ -1,9 +1,24 @@
-import React, { useState } from "react";
-
 function ManufacturersList({manufacturers}){
     return (
         <>
-        <h1>This is a test</h1>
+        <h1 className="mb-3 mt-3">Manufacturers</h1>
+        <table className="table table-striped">
+        <thead>
+          <tr>
+            <th>Manufacturer</th>
+          </tr>
+        </thead>
+        <tbody>
+          {manufacturers.map(manufacturer => {
+            return (
+              <tr key={manufacturer.id}>
+                <td>{ manufacturer.name }</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+
         
         </>
     )
