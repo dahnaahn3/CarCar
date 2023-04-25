d182a1d90b87dff3751f0c56001a4824117fa188
Dahna <dahnaahn16@gmail.com>
2023-04-24 18:21:01 -0700

    added models to sales_rest models.py

--- sales/api/sales_rest/models.py
+++ sales/api/sales_rest/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+class Salesperson(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    employee_id = models.PositiveIntegerField()
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    address = models.TextField()
+    phone_number = models.PositiveIntegerField()
+
+class AutomobileVO(models.Model):
+    vin = models.CharField(max_length=17, unique=True)
+    def __str__(self):
+        return self.vin
+
+class Sale(models.Model):
+    price = models.PositiveIntegerField()
+    automobile = models.ForeignKey(
+        AutomobileVO,
+        related_name="automobile",
+        on_delete=models.CASCADE
+    )
+    salesperson = models.ForeignKey(
+        Salesperson,
+        related_name="salesperson",
+        on_delete=models.CASCADE
+    )
+    customer = models.ForeignKey(
+        Customer,
+        related_name="customer",
+        on_delete=models.CASCADE
+    )
