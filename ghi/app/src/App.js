63248a49213f13579b61b47587e36c71a8026636
Dahna <dahnaahn16@gmail.com>
2023-04-24 17:09:31 -0700

    vehiclemodelform and vehiclemodellist completed

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Nav from './Nav';
+import VehicleModelForm from './VehicleModelForm';
+import VehicleModels from './VehicleModels';
 
 function App() {
   return (
@@ -9,6 +11,12 @@ function App() {
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
+
+          <Route path="models/">
+            <Route path="" element={<VehicleModels />} />
+            <Route path="create" element={<VehicleModelForm />} />
+          </Route>
+
         </Routes>
       </div>
     </BrowserRouter>
---
+++ ghi/app/src/VehicleModels.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react'
+
+function VehicleModels(){
+    const[models, setModels] = useState([]);
+    const fetchData = async () => {
+        const url = 'http://localhost:8100/api/models/'
+        const response = await fetch(url)
+        if(response.ok){
+            const data = await response.json();
+            setModels(data.models)
+        }
+    }
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    return (
+        <div>
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Manufacturer</th>
+                        <th>Picture</th>
+                    </tr>
+                </thead>
+                <tbody>
+            {models.map((model) =>
+                <tr key={model.id}>
+                    <td>{model.name}</td>
+                    <td>{model.manufacturer.name}</td>
+                    <td> <img src={model.picture_url} alt={model.name} /> </td>
+                </tr>
+            )}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default VehicleModels
